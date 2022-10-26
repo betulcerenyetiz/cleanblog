eed618560b8f5f0c57164316360cd4d9af555042
Betül Ceren YETİZ <betulcerenyetiz@gmail.com>
2022-10-26 20:39:25 +0300

    Template Engine added

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import  express  from "express";
+import ejs from "ejs"
 const app= express()
 
+app.set("view engine","ejs")
+app.use(express.static("public"))
+
+
 app.get("/",(req,res)=>{
-    const blog={
-        id: 1, title: "Blog title", description: "Blog description"
-    }
-    res.send(blog)
+    res.render("index")
+})
+app.get("/about",(req,res)=>{
+    res.render("about")
+})
+app.get("/add_post", (req,res)=>{
+    res.render("add_post")
 })
 
 app.listen(3000,()=>{
